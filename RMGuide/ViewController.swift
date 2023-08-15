477ab301af7316c5f31f8640d0dbf59396970a41
Ginza31 <lns-project@yandex.ru>
2023-08-15 10:03:18 +0300

    Initial Commit

---
+++ RMGuide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RMGuide
+//
+//  Created by Динара Шарафутдинова on 15.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
